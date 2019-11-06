6f6663102257884883e73a5a8f70ddd1e483aa2a
Nour Adel <noura7305@gmail.com>
2019-11-06 10:43:09 +0200

    first draft
    printing the details of the song to the console

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,23 @@
-const songsReducers = () => {
-    return [
-        { title: "hey", duration: '4:00'},
-        {title: "heeeey", duratoion : '3:00'}
-    ]
-}
+import { combineReducers } from "redux";
+  const songsReducers = () => {
+  return [
+    { title: "song1", duration: "4:00" },
+    { title: "song2", duration: "3:00" },
+    { title: "song3", duration: "2:00" },
+    { title: "song4", duration: "1:45" }
+  ];
+};
+
+const selectedSongReducers = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+    songs: songsReducers,
+    selectedSong: selectedSongReducers
+  });
+
+
